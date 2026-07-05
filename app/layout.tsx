d4fe89c43a7c0ef1eae7d02d2950d35930775e18
eeeeeeeeeeeeez <diego0966745817@gmail.com>
2026-07-06 01:46:06 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import { SessionProvider } from "next-auth/react";
 export const metadata: Metadata = {
   metadataBase: new URL("https://chat.vercel.ai"),
   title: "Next.js Chatbot Template",
-  description: "HENGBO AI",
+  description: "Next.js chatbot template using the AI SDK.",
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon.ico",
